e548e693f78342270b7728e42f3ddb43a04f15f9
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-27 16:34:11 -0700

    Used regex to grab req, destructured group into unique variables

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const { createServer } = require('net');
 
 const server = createServer(sock => {
   sock.on('data', data => {
-    console.log(data.toString());
+    const req = data.toString().split('\n')[0];
+    const pattern = /(?<method>\w+)\s(?<path>\/\.*)\s(?<protocol>.*)/g;
+
+    const groups = pattern.exec(req).groups;
+    const { method, path, protocol } = groups;
+
+    console.log(method);
+    console.log(path);
+    console.log(protocol);
+
   });
 
 });
