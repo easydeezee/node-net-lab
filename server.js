cf2fd7bd5d2dfa6082b4277198bd819cc912e248
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-28 11:34:33 -0700

    created parsReques.js and test  for that

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const { createServer } = require('net');
+const parseRequest = require('./utilities/parse-request');
 
 const server = createServer(sock => {
   sock.on('data', data => {
     const groups = parseRequest(data);
     const { method, path } = groups;
     console.log(method, path);
+
+    if(path === '/' && method === 'GET') {
+      console.log('get requested on path /');
+    } else if (path === '/red.html' && method === 'GET') {
+      console.log('requested red.html method get');
+      const content = readFile(path);
+      sock.write(content);
+    }
+
   });
 
 });
 
-function parseRequest(data) {
-  const req = data.toString().split('\n')[0];
-  const method = req.split(' ')[0];
-  const path = req.split(' ')[1];
-  return { method, path };
+function readFile(path) {
+  fs.readFile(path, (err, content) => {
+
+  });
 }
 
 server.listen(7890);
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,26 +1,8 @@
 const request = require('supertest');
-const { server, parseRequest } = require('../server');
+const { server } = require('../server');
 // const fs = require('fs');
 
-describe('parseRequest function', () => {
-  it('returns correct method and path', () => {
-    const parsed = parseRequest('POST /red HTTP/1.1');
-    expect(parsed.method).toEqual('POST');
-    expect(parsed.path).toEqual('/red');
-  });
 
-  it('returns correct method and path', () => {
-    const parsed = parseRequest('GET / HTTP/1.1');
-    expect(parsed.method).toEqual('GET');
-    expect(parsed.path).toEqual('/');
-  });
-
-  it('returns correct method and path', () => {
-    const parsed = parseRequest('POST /blue HTTP/1.1');
-    expect(parsed.method).toEqual('POST');
-    expect(parsed.path).toEqual('/blue');
-  });
-});
 
 // describe('server routes', () => {
 //   it('returns 404 not found if path not found', () => {
