b49b3d1d944f551d914a116b360ebd9c23db7613
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-28 14:57:39 -0700

    Create server.js

--- server.js
+++ server.js
@@ -1,29 +1,85 @@
 const { createServer } = require('net');
-const parseRequest = require('./utilities/parse-request');
+const { parseRequest } = require('./utilities/parse-request');
+const fs = require('fs');
+
+const getMethods = {
+  '/': {
+    'file': '/hi.txt',
+    'type': 'text/plain',
+    'status': '200'
+  },
+  '/red': {
+    'file': '/red.html',
+    'type': 'text/html',
+    'status': '200'
+  },
+  '/green': {
+    'file': '/green.html',
+    'type': 'text/html',
+    'status': '200'
+  },
+  '/blue': {
+    'file': '/blue.html',
+    'type': 'text/html',
+    'status': '200'
+  },
+  '/dog': {
+    'file': '/dog.json',
+    'type': 'application/json',
+    'status': '200'
+  }
+};
+
+const postMethods = {
+  '/': {
+    'file': '/sorry.txt',
+    'type': 'text/plain',
+    'status': '400'
+  }
+};
+
+function getInfoFromPath(method, path) {    
+  if(method === 'GET' && getMethods[path]) {
+    return getMethods[path];
+  } else if(method === 'POST' && getMethods[path]) {
+    return postMethods[path];
+  } else {
+    return null;
+  }
+}
+
+function makeHTTPPacket(content, pathInfo) {
+  return `HTTP/1.1 ${pathInfo.status} OK
+  Accept-Ranges: bytes
+  Content-Length: ${content.length}
+  Content-Type: ${pathInfo.type}
+
+  ${content.toString()}
+  `;
+}
 
 const server = createServer(sock => {
   sock.on('data', data => {
     const groups = parseRequest(data);
     const { method, path } = groups;
-    console.log(method, path);
 
-    if(path === '/' && method === 'GET') {
-      console.log('get requested on path /');
-    } else if (path === '/red.html' && method === 'GET') {
-      console.log('requested red.html method get');
-      const content = readFile(path);
-      sock.write(content);
+    let pathInfo = getInfoFromPath(method, path);
+    if(!pathInfo) {
+      pathInfo = {
+        'file': '/not-found.html',
+        'type': 'text/html',
+        'status': '404'
+      };
     }
 
+    fs.readFile(`./public${pathInfo.file}`, (err, content) => {
+      if(err) console.error(err);
+      sock.write(makeHTTPPacket(content, pathInfo));
+      sock.end();
+    });
+    
   });
-
 });
-
-function readFile(path) {
-  fs.readFile(path, (err, content) => {
-
-  });
-}
 
 server.listen(7890);
 
