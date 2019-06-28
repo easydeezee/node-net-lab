a80c41118450ccd0728b49408d6a02320e514e29
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-28 10:48:53 -0700

    working tests for parseRequest function

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ const { createServer } = require('net');
 const server = createServer(sock => {
   sock.on('data', data => {
     const req = data.toString().split('\n')[0];
-    console.log(req);
     // const groups = pattern.exec(req);
     // const { method, path } = groups;
     // console.log(method, path);
+    console.log(parseRequest(req));
 
   });
 
-  function parseRequest(req) {
-    const pattern = /(?<method>\w+)\s(?<path>\/\.*)\s(?<protocol>.*)/g;
-    return pattern.exec(req).groups;
-  } 
-  console.log(parseRequestreq);
-
 });
+
+function parseRequest(req) {
+  const pattern = /(?<method>\w+)\s(?<path>\/.*)\s(?<protocol>.*)/g;
+  return pattern.exec(req).groups;
+}
 
 server.listen(7890);
 
-module.exports = server;
+module.exports = { server, parseRequest };
--- tests/server.test.js
+++ tests/server.test.js
@@ -4,19 +4,31 @@ const { server, parseRequest } = require('../server');
 
 describe('parseRequest function', () => {
   it('returns correct method and path', () => {
+    const parsed = parseRequest('POST /red HTTP/1.1');
+    expect(parsed.method).toEqual('POST');
+    expect(parsed.path).toEqual('/red');
+  });
+
+  it('returns correct method and path', () => {
     const parsed = parseRequest('GET / HTTP/1.1');
     expect(parsed.method).toEqual('GET');
     expect(parsed.path).toEqual('/');
   });
-});
 
-describe('server routes', () => {
-  it('returns 404 not found if path not found', () => {
-    return request(server)
-      .get('/badPath')
-      .then(res => {
-        expect(res.statusCode).toEqual(404);
-        expect(res.text).toEqual(expect.stringContaining('not found'));
-      });
+  it('returns correct method and path', () => {
+    const parsed = parseRequest('POST /blue HTTP/1.1');
+    expect(parsed.method).toEqual('POST');
+    expect(parsed.path).toEqual('/blue');
   });
 });
+
+// describe('server routes', () => {
+//   it('returns 404 not found if path not found', () => {
+//     return request(server)
+//       .get('/badPath')
+//       .then(res => {
+//         expect(res.statusCode).toEqual(404);
+//         expect(res.text).toEqual(expect.stringContaining('not found'));
+//       });
+//   });
+// });
